9d7ae226f88cd3c15efc1c3633b0a37a79964eed
yifanzbox <yifanzbox@gmail.com>
2025-04-22 18:04:55 +0000

    Add cookie parsing

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const path = require('path');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', (req, res) => {
